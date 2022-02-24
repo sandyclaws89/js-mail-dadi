8f4c7cebb48cefd37a158eeef7cd3f19e2d36bf5
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-02-24 17:23:41 +0100

    Esercizio dadi terminato (con soli due dadi, ora faccio gli html di tutti e due gli esercizi poi aggiungo altri dadi)

---
+++ dadi/js/script.js
@@ -0,0 +1,29 @@
+
+
+
+
+let userDice = Math.floor(Math.random()*(0-6)) +7;
+console.log(userDice);
+
+let computerDice = Math.floor(Math.random()*(0-6)) +7;
+console.log(computerDice);
+
+if (userDice > computerDice){
+    console.log(`Hai vinto!`)
+} else if (userDice == computerDice){
+    console.log(`Pari!`)
+}
+else{
+    console.log(`Hai perso!`)
+}
+
+
+
+
+
+
+// let numbers = Math.floor(Math.random);
+
+
+
+
