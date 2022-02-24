f63e7fb90efe13275e6ad4e01b081c9c9c924244
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-02-24 16:47:38 +0100

    Finito esercizio email

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 let emailList =["test0@gmail","test1@gmail","test2@gmail","test3@gmail","test4@gmail","test5@gmail","test6@gmail","test7@gmail","test8@gmail","test9@gmail"];
-console.log(emailList);
 
 let userEmail = prompt(`Inserire l'email`);
-// console.log(userEmail);
+let PresentEmail = false;
 
-for (let i=0; i <=emailList; i++) {
+for (let i = 0; i < emailList.length; i++) {
 
-    console.log(emailList[i])
-    // if (emailList[i] == userEmail){
-       
-    // }
-    // console.log ('ok')
+    if (emailList[i] == userEmail){
+        PresentEmail=true;
+    }
+
 }
 
-
+if (PresentEmail ==true){
+    console.log (`Login ${userEmail}`);
+    
+} else {
+    console.log (`Email non presente nel database`);
+}
