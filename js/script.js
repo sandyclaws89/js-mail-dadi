21d8d71ac66e056634aa3d76470af3230ab4d84e
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-02-24 16:12:50 +0100

    impostazione esercizio email

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let emailList =["test0@gmail","test1@gmail","test2@gmail","test3@gmail","test4@gmail","test5@gmail","test6@gmail","test7@gmail","test8@gmail","test9@gmail"];
+console.log(emailList);
+
+let userEmail = prompt(`Inserire l'email`);
+// console.log(userEmail);
+
+for (let i=0; i <=emailList; i++) {
+
+    console.log(emailList[i])
+    // if (emailList[i] == userEmail){
+       
+    // }
+    // console.log ('ok')
+}
+
+
