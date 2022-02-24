5146e2cde1da9514b6b65080e86b7cd5f1b12616
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-02-24 17:45:09 +0100

    Non riesco a far comunicare html e js

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,19 +1,25 @@
 let emailList =["test0@gmail","test1@gmail","test2@gmail","test3@gmail","test4@gmail","test5@gmail","test6@gmail","test7@gmail","test8@gmail","test9@gmail"];
 
-let userEmail = prompt(`Inserire l'email`);
-let PresentEmail = false;
+let userEmail = document.querySelector(`#email-utente`);
+console.log(userEmail);
+// NON CAPISCO PERCHE' NON MI DA IN CONSOLE SOLO IL VALORE DI USEREMAIL MA TUTTO IL TAG
 
-for (let i = 0; i < emailList.length; i++) {
 
-    if (emailList[i] == userEmail){
-        PresentEmail=true;
-    }
+// let PresentEmail = false;
 
-}
 
-if (PresentEmail ==true){
-    console.log (`Login ${userEmail}`);
+
+// for (let i = 0; i < emailList.length; i++) {
+
+//     if (emailList[i] == userEmail){
+//         PresentEmail=true;
+//     }
+
+// }
+
+// if (PresentEmail ==true){
+//     console.log (`Login ${userEmail}`);
     
-} else {
-    console.log (`Email non presente nel database`);
-}
+// } else {
+//     console.log (`Email non presente nel database`);
+// }
